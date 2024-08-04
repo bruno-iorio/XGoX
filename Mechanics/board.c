d91c094f8598448f28abec4872b3a80909e6b4da
bruno-iorio <bruno.fernandes.iorio@gmail.com>
2024-08-04 22:12:50 +0200

    restart

--- Mechanics/board.c
+++ Mechanics/board.c
@@ -1,72 +1,47 @@
+#include "board.h"
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
-#include<errnor.h>
+#include<errno.h>
 
-#include<string.h>
-#include<board.h>
-
-void update_turn(game* game){
-	if (game.turn==WHITE){
-		game->turn = BLACK;
-	} else{
-		game->turn = WHITE;
-	}
-
-}
-game* create_game(int handicap,int komi){
-	game* game = (game *) malloc(sizeof(game));
-	game->turn = BLACK;
-	game->komi = komi;
-	game->handicap = handicap;
-
-	game->board = (int **) malloc(BOARD_SIZE*sizeof(int*));
-	
-	for(int i = 0; i < BOARD_SIZE, i++ ){
-		game->board[i] = (int *) malloc(BOARD_SIZE*sizeof(int));
-		for(int j = 0; j < BOARD_SIZE, j++){
-			game->board[i][j] = EMPTY;
-		}
-	}
-	
-	game->groups = (group*) malloc(sizeof(group))
-
-	return game;
-}
-void update_groups_size(game* game,int inc){
-	group_new = (group*) realloc(game->groups,sizeof(group)*(inc + number_groups))
-        game->groups = group_new;
+int main(){
+  board* Board = create_board();
+  print_board(Board);
+  return 0;
 }
 
-int add_stone(game* game, int color, int x, int y){
-	if(color != WHITE && color != BLACK){
-		perror("Invalid Color! Please, choose white or black");
-		return -1;
-	}
-	if(x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE){
-		perror("Invalid coordinates! Please, valid coordinates");
-		return -1;
-	}
-	game->board[x][y] = color;	
-	return 0;
+board* create_board(void){
+  board* Board = (board*) malloc(sizeof(board*));
+  Board->grid = (int**) malloc(BOARDSIZE*sizeof(int*));
+
+  for(int i = 0; i < BOARDSIZE; i++){
+    Board->grid[i] = (int *) malloc(BOARDSIZE*sizeof(int));
+  }
+  
+  for(int i = 0; i <  BOARDSIZE; i++){
+    for(int j = 0; j <  BOARDSIZE; j++){
+      if (i == 0 || i == BOARDSIZE - 1 || j == 0 || j == BOARDSIZE-1){
+        Board->grid[i][j] = 3;
+      } else {
+        Board->grid[i][j] = 0;
+      }
+    }
+  }
+  return Board;
 }
 
-int remove_stone(*game, int x, int y){
-	if(x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE){
-		perror("Invalid coordinates! Please, valid coordinates");
-		return -1;
-	}
-	game->board[x][y] = EMPTY;
-	return 0;
+void print_board(board* Board){
+  for(int i = 0; i < BOARDSIZE; ++i){
+    for(int j = 0; j < BOARDSIZE; ++j){
+      printf("%d ",Board->grid[i][j]);
+    }
+    printf("\n");
+  }
 }
 
-int check_capture(group* group);
-	if(group == NULL){
-		perror("Group has null value");
-		return -1
-	}
-	if (group->liberties==0){ //captured
-		return 1;
-	}
-	return 0; //not captured
-
+int play_move(board* Board,int color, int x, int y){
+  if(x <= 0 || x >= BOARDSIZE-1 || y <= 0 || y >= BOARDSIZE-1){
+    return -1; //error
+  }
+  Board->grid[x][y] = color;
+  return 0;
+}
--- Mechanics/board.h
+++ Mechanics/board.h
@@ -1,50 +1,19 @@
 #ifndef BOARD_H
 #define BOARD_H
-#define BLACK 1
-#define WHITE 2
-#define EMPTY 0
-#define BOARD_SIZE 19
+#define BOARDSIZE 21
+enum Color{
+  Empty = 0,
+  Black = 1,
+  White = 2,
+  Wall = 3, 
+};
 
-typedef struct group{
-int color;
-int liberties;  // Important for capture race
-int eyes_count; // >= 2 eyes is confirmed alive!
-		
-		// To define a group, we just need 1 stone of this group:
-int x_pos;      
-int y_pos;
-} group
+typedef struct board{
+  int** grid;
+  int turn; //1 if black, 2 if white
+} board;
 
-typedef struct game{
-int** board;   // This will be the board
-int handicap;  // How many extra-stones to be played with
-int komi;      // How much komi for white
-int turn;      // whose turn is it in this game
-
-group* groups;
-int number_groups;
-} game;
-
-typedef struct stone{
-int x_pos;
-int y_pos;
-
-} stone;
-
-group* find_group(int,int);
-game* create_game(int,int);
-void update_turn(game*);
-void update_groups_size(game*,int);
-int add_stone(game*,int,int,int); // doesnt matter the turn!
-int remove_stone(game*,int,int);
-int check_capture(group*);
-
-
-#endif //BOARD_H
-
-
-
-
-
-
-
+int play_move(board*, int,int,int);
+void print_board(board*);
+board* create_board();
+#endif // BOARD_H
